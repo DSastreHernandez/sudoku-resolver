fbac5df6a8af3b779b7993731f3db67eb514e342
noecrespi <ncrespipomar@cifpfbmoll.eu>
2021-11-08 21:04:19 +0100

    Comprovación de filas

---
+++ check_rows_in_sudoku.py
@@ -0,0 +1,38 @@
+def check_rows_in_sudoku(sudoku):
+    for row in sudoku:
+        for number in row:
+            if not row.count(number) == 1:
+                return False
+    return True
+
+
+if __name__ == '__main__':
+    assert check_rows_in_sudoku([[1, 2, 3],
+                                 [2, 3, 1],
+                                 [3, 1, 2]]) == True
+    assert check_rows_in_sudoku([[1, 2, 3, 4],
+                                 [2, 3, 1, 3],
+                                 [3, 1, 2, 3],
+                                 [4, 4, 4, 4]]) == False
+    assert check_rows_in_sudoku([[1, 2, 3, 4],
+                                 [2, 3, 1, 4],
+                                 [4, 1, 2, 3],
+                                 [3, 4, 1, 2]]) == True
+    assert check_rows_in_sudoku([[1, 2, 3, 4, 5],
+                                 [2, 3, 1, 5, 6],
+                                 [4, 5, 2, 1, 3],
+                                 [3, 4, 5, 2, 1],
+                                 [5, 6, 4, 3, 2]]) == True
+    assert check_rows_in_sudoku([[1, 2, 3, 4, 5],
+                                 [2, 3, 1, 5, 6],
+                                 [4, 5, 2, 1, 3],
+                                 [3, 4, 5, 2, 1],
+                                 [5, 6, 4, 3, 2]]) == True
+    assert check_rows_in_sudoku([['a', 'b', 'c'],
+                                 ['b', 'c', 'a'],
+                                 ['c', 'a', 'b']]) == True
+    assert check_rows_in_sudoku([[1, 1.5],
+                                 [1.5, 1]]) == True
+    assert check_rows_in_sudoku([[1, 0, 0, 0],
+                                 [0, 1, 0],
+                                 [0, 0, 0, 1]]) == False
