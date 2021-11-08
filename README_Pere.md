9963c5af8970f39a8e8c10f2f7ed36b922af983c
Pere-borras <pborrasexposito@cifpfbmoll.eu>
2021-11-08 19:41:52 +0100

    Primer commit en la rama Pere-features

---
+++ README_Pere.md
@@ -0,0 +1,5 @@
+# Tarea 5 - Pere Antoni Borràs Expósito
+
+### Programa de Python de un Sudoku modular
+
+En esta rama se encuentra el programa **"check_sudoku_cuadrado.py"**, que devuelve un resultado booleano según si el input es (primero), una lista y (segundo) si es cuadrado.
---
+++ check_sudoku_cuadrado.py
@@ -0,0 +1,25 @@
+def check_sudoku_cuadrado(sudoku):
+    for element in sudoku:
+        if len(element) != len(sudoku):
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    assert check_sudoku_cuadrado([[1, 2, 3], [2, 3, 1], [3, 1, 2]]) == True
+    assert check_sudoku_cuadrado([[1, 2, 3, 4], [2, 3, 1, 3], [
+        3, 1, 2, 3], [4, 4, 4, 4]]) == True
+    assert check_sudoku_cuadrado([[1, 2, 3, 4], [2, 3, 1, 4], [
+        4, 1, 2, 3], [3, 4, 1, 2]]) == True
+    assert check_sudoku_cuadrado([[1, 2, 3, 4, 5], [2, 3, 1, 5, 6], [4, 5, 2, 1, 3], [
+        3, 4, 5, 2, 1], [5, 6, 4, 3, 2]]) == True
+    assert check_sudoku_cuadrado([[1, 2, 3, 4, 5], [2, 3, 1, 5, 6], [4, 5, 2, 1, 3], [
+        3, 4, 5, 2, 1], [5, 6, 4, 3, 2]]) == True
+    assert check_sudoku_cuadrado([['a', 'b', 'c'],
+                                  ['b', 'c', 'a'],
+                                  ['c', 'a', 'b']]) == True
+    assert check_sudoku_cuadrado([[1, 1.5],
+                                  [1.5, 1]]) == True
+    assert check_sudoku_cuadrado([[1, 0, 0, 0],
+                                  [0, 1, 0],
+                                  [0, 0, 0, 1]]) == False
