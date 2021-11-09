a0d5d2c35a3130e9e1d2e2a2cb024f0383e4cc80
Pere-borras <pborrasexposito@cifpfbmoll.eu>
2021-11-09 19:44:10 +0100

    Añadidos comentarios dentro del programa

--- casos_test_sudoku.py
+++ casos_test_sudoku.py
@@ -1,4 +1,7 @@
 if __name__ == '__main__':
+
+    # Casos test de "check_columns_in_sudoku"
+
     assert check_columns_in_sudoku([[1, 2, 3],
                                     [2, 3, 1],
                                     [3, 1, 2]]) == True
@@ -28,6 +31,8 @@ if __name__ == '__main__':
     assert check_columns_in_sudoku([[1, 0, 0, 0],
                                     [0, 1, 0],
                                     [0, 0, 0, 1]]) == False
+
+# Casos test de "check_rows_in_sudoku"
 
 if __name__ == '__main__':
     assert check_rows_in_sudoku([[1, 2, 3],
@@ -60,6 +65,8 @@ if __name__ == '__main__':
                                  [0, 1, 0],
                                  [0, 0, 0, 1]]) == False
 
+# Casos test de "check_sudoku_cuadrado"
+
 if __name__ == '__main__':
     assert check_sudoku_cuadrado([[1, 2, 3], [2, 3, 1], [3, 1, 2]]) == True
     assert check_sudoku_cuadrado([[1, 2, 3, 4], [2, 3, 1, 3], [
@@ -78,6 +85,8 @@ if __name__ == '__main__':
     assert check_sudoku_cuadrado([[1, 0, 0, 0],
                                   [0, 1, 0],
                                   [0, 0, 0, 1]]) == False
+
+# Casos test de "check_sudoku"
 
     assert check_sudoku([[1, 2, 3],
                         [2, 3, 1],
